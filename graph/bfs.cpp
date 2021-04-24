4a127eeeba4b0598d10c490edb3e42563b4a82af
shubhdeep123 <shubhdeepsharma213@gmail.com>
2021-04-25 00:19:31 +0530

    bfs

---
+++ graph/bfs.cpp
@@ -0,0 +1,108 @@
+// #include <iostream>
+// #include<queue>
+// using namespace std;
+
+// void print(int **edges, int n, int sv) {
+//     queue<int> pendingVertices;
+//     bool *visited = new bool[n];
+//     for (int i=0; i<n; i++) {
+//         visited[i] = false;
+//     }
+
+//     pendingVertices.push(sv);
+//     visited[sv] = true;
+//     while (!pendingVertices.empty()) {
+//         int currentVertex = pendingVertices.front();
+//         pendingVertices.pop();
+//         cout << currentVertex << endl;
+//         for (int i=0; i<n; i++) {
+//             if (i == currentVertex) {
+//                 continue;
+//             }
+
+//             if (edges[currentVertex][i] == 1 && !visited[i]) {
+//                 pendingVertices.push(i);
+//                 visited[i] = true;
+//             }
+//         }
+//     }
+
+//     delete [] visited;
+// }
+
+// int main() {
+//     // n is number vertices and e is edges
+//     int n,e;
+//     cin >> n >> e;
+
+//     int **edges = new int*[n];
+//     for (int i=0; i<n; i++) {
+//         edges[i] = new int[n];
+//         for(int j=0; j<n; j++) {
+//             edges[i][j] = 0;
+//         }
+//     }
+
+//     for (int i=0; i<e; i++) {
+//         int f,s;
+//         cin >> f >> s;
+//         edges[f][s] = 1;
+//         edges[s][f] = 1;
+//     }
+
+//     print(edges,n,0);
+// }
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void printBFS(int **edges, int n, int sv) {
+    queue<int> pendingVertices;
+    bool *visited = new bool[n];
+    for (int i=0; i<n; i++) {
+        visited[i] = false;
+    }
+
+    pendingVertices.push(sv);
+    visited[sv] = true;
+    while (!pendingVertices.empty()) {
+        int currentVertex = pendingVertices.front();
+        pendingVertices.pop();
+        cout << currentVertex << endl;
+        for (int i=0; i<n; i++) {
+            if (i==currentVertex) {
+                continue;
+            }
+
+            if (edges[currentVertex][i] == 1 && !visited[i]) {
+                pendingVertices.push(i);
+                visited[i] = true;
+            }
+        } 
+    }
+
+    delete [] visited;
+}
+
+int main () {
+    int n,e;
+    cin >> n >> e;
+
+    int **edges = new int*[n];
+    for (int i=0; i<n; i++) {
+        edges[i] = new int[n];
+        for (int j=0; j<n; j++) {
+            edges[i][j] = 0;
+        }
+    }
+
+    for (int i=0; i<e; i++) {
+        int f,s;
+        cin >> f >> s;
+        edges[f][s] = 1;
+        edges[s][f] = 1;
+    }
+
+    printBFS(edges,n,0);
+}
